26bb76905310a2bdec6e357b3c699a3ed5b9d308
Ereno <mamustafa@wp.pl>
2021-02-07 17:13:12 +0100

    + added PossibleWordsPL.js (alpha version) and test PossibleHelper.js

---
+++ PossibleWordsPL.js
@@ -0,0 +1,70 @@
+var PossibleWordsPL = [
+	{
+		"length": 3,
+		"words": ["łuk"]
+	}, 
+	{
+		"length": 4,
+		"words": ["Azja"]
+	}, 
+	{
+		"length": 5,
+		"words": ["grill", "gołąb", "Sonic"]
+	}, 
+	{
+		"length": 6,
+		"words": ["łopata", "miotła", "orbita"]
+	}, 
+	{
+		"length": 7,
+		"words": ["bazooka", "speaker"]
+	}, 
+	{
+		"length": 8,
+		"words": ["babeczka", "podstawa", "antylopa", "recepcja"]
+	}, 
+	{
+		"length": 9,
+		"words": ["King Kong", "spaghetti", "korniszon", "marynarka", "Wolverine"]
+	}, 
+	{
+		"length": 10,
+		"words": ["Steve Jobs"]
+	}, 
+	{
+		"length": 11,
+		"words": ["konik polny"]
+	}, 
+	{
+		"length": 12,
+		"words": []
+	}, 
+	{
+		"length": 13,
+		"words": []
+	}, 
+	{
+		"length": 14,
+		"words": []
+	}, 
+	{
+		"length": 15,
+		"words": ["Mark Zuckerberg"]
+	}, 
+	{
+		"length": 16,
+		"words": []
+	}, 
+	{
+		"length": 17,
+		"words": []
+	}, 
+	{
+		"length": 18,
+		"words": []
+	}, 
+	{
+		"length": 19,
+		"words": []
+	}
+]
---
+++ SkribblHelperPL.js
@@ -0,0 +1,3 @@
+console.log("test 1");
+console.log(PossibleWordsPL);
+console.log("test 2");
